5334ac7acfe09897ba758e9cf6a784ccb9bc4ecd
wngnlwngnl <hyeokjin0126@naver.com>
2026-08-07 20:44:16 +0900

    Update 01-vpc.sh

--- 1과제/02/01-vpc.sh
+++ 1과제/02/01-vpc.sh
@@ -14,15 +14,19 @@ aws ec2 modify-vpc-attribute --vpc-id $VPC --enable-dns-hostnames '{"Value":true
 IGW=$(aws ec2 create-internet-gateway --tag-specifications 'ResourceType=internet-gateway,Tags=[{Key=Name,Value=book-igw}]' --query InternetGateway.InternetGatewayId --output text)
 aws ec2 attach-internet-gateway --internet-gateway-id $IGW --vpc-id $VPC
 
-mksub(){ aws ec2 create-subnet --vpc-id $VPC --cidr-block $2 --availability-zone $3 \
-  --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=$1}]" --query Subnet.SubnetId --output text; }
+mksub(){ 
+  aws ec2 create-subnet --vpc-id $VPC --cidr-block $2 --availability-zone $3 \
+    --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=$1}]" --query Subnet.SubnetId --output text
+}
 
 PUB_C=$(mksub wskorea26-pub-subnet-c   172.16.1.0/24   ap-northeast-2c)
 PUB_D=$(mksub wskorea26-pub-subnet-d   172.16.2.0/24   ap-northeast-2d)
 PRIV_C=$(mksub wskorea26-priv-subnet-c 172.16.201.0/24 ap-northeast-2c)
 PRIV_D=$(mksub wskorea26-priv-subnet-d 172.16.202.0/24 ap-northeast-2d)
 
-for s in $PUB_C $PUB_D; do aws ec2 modify-subnet-attribute --subnet-id $s --map-public-ip-on-launch; done
+for s in $PUB_C $PUB_D; do 
+  aws ec2 modify-subnet-attribute --subnet-id $s --map-public-ip-on-launch
+done
 
 EIP_C=$(aws ec2 allocate-address --domain vpc --query AllocationId --output text)
 NGW_C=$(aws ec2 create-nat-gateway --subnet-id $PUB_C --allocation-id $EIP_C \
@@ -34,7 +38,10 @@ NGW_D=$(aws ec2 create-nat-gateway --subnet-id $PUB_D --allocation-id $EIP_D \
 
 aws ec2 wait nat-gateway-available --nat-gateway-ids $NGW_C $NGW_D
 
-mkrtb(){ aws ec2 create-route-table --vpc-id $VPC --tag-specifications "ResourceType=route-table,Tags=[{Key=Name,Value=$1}]" --query RouteTable.RouteTableId --output text; }
+mkrtb(){ 
+  aws ec2 create-route-table --vpc-id $VPC --tag-specifications "ResourceType=route-table,Tags=[{Key=Name,Value=$1}]" --query RouteTable.RouteTableId --output text
+}
+
 PUBLIC_RT=$(mkrtb wskorea26-public-rtb)
 PRIVATE_RT_C=$(mkrtb wskorea26-private-rtb-c)
 PRIVATE_RT_D=$(mkrtb wskorea26-private-rtb-d)
