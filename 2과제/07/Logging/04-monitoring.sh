68cacf5f2be62932269f87b6fb84f370c70ae547
wngnlwngnl <hyeokjin0126@naver.com>
2026-08-08 13:57:40 +0900

    Update 04-monitoring.sh

--- 2과제/07/Logging/04-monitoring.sh
+++ 2과제/07/Logging/04-monitoring.sh
@@ -142,11 +142,8 @@ monitoring:
   serviceMonitor:
     enabled: false
 EOF
+
 helm upgrade --install o11y-loki grafana/loki -n monitoring -f ./loki-values.yaml --wait --timeout 10m
-
-
-
-
 
 
 cat <<EOF >> otel-values.yaml
