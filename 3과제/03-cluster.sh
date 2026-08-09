a6636fce6320f50de29005dc2875eeb489125b8f
wngnlwngnl <hyeokjin0126@naver.com>
2026-08-09 12:27:33 +0900

    Update 03-cluster.sh

--- 3과제/03-cluster.sh
+++ 3과제/03-cluster.sh
@@ -67,4 +67,7 @@ managedNodeGroups:
         cloudWatch: true
 EOF
 
+curl --silent --location "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+
 eksctl create cluster -f cluster.yaml
