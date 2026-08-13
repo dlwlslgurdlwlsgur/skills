563b18d5e59fd7b86be602a6ad801428afc81afc
wngnlwngnl <hyeokjin0126@naver.com>
2026-08-13 20:31:32 +0900

    Update mark.sh

--- 2과제/07/Logging/mark.sh
+++ 2과제/07/Logging/mark.sh
@@ -54,13 +54,12 @@ RESP=$(curl -s "http://$(aws elbv2 describe-load-balancers --names o11y-app-alb 
 kubectl port-forward -n monitoring svc/o11y-loki 3100:3100 > /dev/null 2>&1 &
 PF=$!
 sleep 2
-# * 아래 명령어를 통해 로그가 정상적으로 조회되는지 확인합니다. 선수는 1분간 원하는 만큼 해당 명령어를 원하는 만큼 실행할 수 있습니다.
 curl -s -G http://localhost:3100/loki/api/v1/query_range --data-urlencode 'query={k8s_namespace_name="o11y"} | json | level="ERROR"' --data-urlencode "start=$(date -d '3 minutes ago' +%s)000000000" --data-urlencode "end=$(date +%s)000000000" --data-urlencode 'limit=20' | jq -r '.data.result[].values[][1]'
+kill $PF 2>/dev/null
 # {"ts":"2026-05-31T12:26:32.805Z","level":"ERROR","msg":"log generated","req_id":"75d9896e-db0a-4b98-b859-ea6df730b04f"}
 # {"ts":"2026-05-31T12:26:32.805Z","level":"ERROR","msg":"log generated","req_id":"9beba665-fb2b-4543-ade4-1cb87e793a44"}
+# 명령어를 통해 로그가 정상적으로 조회되는지 확인합니다. 선수는 1분간 원하는 만큼 해당 명령어를 원하는 만큼 실행할 수 있습니다.
 # 출력되는 로그 중 3분 이내로 기록된 로그가 있다면 득점 인정.
-# * 이후, 아래 명령어를 통해 포트포워딩 중인 프로세스를 종료합니다.
-kill $PF 2>/dev/null
 
 
 echo 'manual marking'
